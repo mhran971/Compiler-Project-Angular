3ddfb296f1bad9f527d7013e0ff7387a947780be
Mahran_AD <mhranabwdqt971@gmail.com>
2025-05-19 23:15:38 +0300

    creating raw class

---
+++ code/src/SymmboleTable/Raw.java
@@ -0,0 +1,24 @@
+package SymmboleTable;
+
+public class Raw {
+    String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    String value;
+}
+
